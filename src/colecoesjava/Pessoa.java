2fc3f66823199c36503393675af73fe0c374f9d6
newjr0308 <94936542+newjr0308@users.noreply.github.com>
2025-06-09 23:59:12 -0300

    Criação da classe Pessoa

---
+++ src/colecoesjava/Pessoa.java
@@ -0,0 +1,26 @@
+package colecoesjava;
+
+public class Pessoa {
+	private String nome;
+	
+	public Pessoa() {
+		nome = "Fulano";
+	}
+	
+	public Pessoa(String nome) {
+		this.nome = nome;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+		if(true) {
+			
+		}
+	}
+	
+	
+}
--- src/colecoesjava/Principal.java
+++ src/colecoesjava/Principal.java
@@ -4,7 +4,10 @@ public class Principal {
 
 	public static void main(String[] args) {
 		System.out.println("Olá mundo");
-
+		Pessoa pessoa = new Pessoa();
+		Pessoa pessoa2 = new Pessoa("Renan");
+		System.out.println(pessoa.getNome());
+		System.out.println(pessoa2.getNome());
 	}
 
 }
